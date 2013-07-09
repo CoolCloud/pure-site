f63ea0d029ebc101e8a4c1d45d17f7ed1fcd2996
chensy <chensy0203@163.com>
2013-07-09 23:06:45 +0800

    first translate
    translate base mdule

--- config/index.js
+++ config/index.js
@@ -17,8 +17,8 @@ module.exports = Object.freeze({
 
     version: require('../package').version,
 
-    ga     : 'UA-41480445-1',
+    //ga     : 'UA-41480445-1',
     pure   : require('./pure'),
-    typekit: 'ajf8ggy',
+    //typekit: 'ajf8ggy',
     yui    : require('./yui')
 });
